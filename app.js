7f72f7cb32c94866ca12a5db92747dcfcaf8a728
[Sushil Awale] <[sushil.awale@deerwalk.edu.np]>
2018-06-04 13:19:10 +0545

    Bootstrap and JQuery files added

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/userRoute');
+var loginRouter = require('./routes/login');
+var userRouter = require('./routes/userRoute');
 
 var app = express();
 
@@ -19,8 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', loginRouter);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/login.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('login',
+        {
+            title: 'Login'
+        }
+    );
+});
+
+module.exports = router;
