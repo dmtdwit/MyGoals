7de10d739d92a986eb1f8595c44da91aa933a825
[Sushil Awale] <[sushil.awale@deerwalk.edu.np]>
2018-06-05 16:41:29 +0545

    Show all users from database in user dashboard page

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var models  = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
+    res.redirect('dashboard');
 });
 
 router.get('/profile', function(req, res, next) {
@@ -12,6 +13,17 @@ router.get('/profile', function(req, res, next) {
             title: 'User Profile'
         }
     );
+});
+
+router.get('/dashboard', function(req, res, next) {
+
+    models.User.findAll({
+    }).then(function(users) {
+        res.render('user/dashboard', {
+            title: 'Dashboard | Users',
+            users: users
+        });
+    });
 });
 
 module.exports = router;
