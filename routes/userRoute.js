339994b5927dce626bbb8c032b3fc79d5ad6285e
pathaksabin88 <pathaksabin88@gmail.com>
2018-06-05 13:41:43 +0545

    "Testing Getting Values From Model"

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,19 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
+    var user_lists = models.User.findAll();
+    console.log(user_lists);
+    console.log("We are here at User page");
+    res.render('user.ejs',
+        {
+            title: 'Users List',
+            user: 'Sabin Pathak'
+        }
+    );
 });
+
 
 module.exports = router;
