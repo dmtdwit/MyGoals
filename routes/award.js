1ba74bc3a67030070d75e22c0b6c9ed032b04a85
babusabin <pathaksabin88@gmail.com>
2018-06-08 13:34:29 +0545

    "Completed CRUD in Award model"

--- routes/award.js
+++ routes/award.js
@@ -23,24 +23,52 @@ router.get('/create', function (req, res, next) {
 
 
 router.get('/save', function (req, res, next) {
-    console.log("Title is ",req.param('title'));
    models.Award.create({
-       title: req.param('title')
+       title: req.query['title'],
+       iconName: req.query['iconName'],
+       iconColor: req.query['iconColor']
    }).then(function () {
        res.redirect('/award')
    });
 });
 
 router.get('/edit', function (req, res, next) {
-    var id = req.param('id');
-    console.log("Id is ",id);
+    var id = req.query['id'];
+    console.log("Id is Edit ",id);
     // console.log("Award is ", models.Award.findById(id));
-    models.Award.find({where:{id: id}}).then(function (award) {
-        res.render('award/edit.ejs',{
+    models.Award.findOne({where:{id: id}}).then(function (award) {
+        res.render('award/edit.ejs', {
             title: 'Edit Award | My Goals',
             award: award
         });
     });
+});
+
+router.get('/update', function (req, res, next) {
+    var id = req.query['id'];
+    models.Award.find({where:{
+        id: id
+    }}).then(function(award) {
+       if(award){
+           award.updateAttributes({
+               title: req.query['title'],
+               iconName: req.query['iconName'],
+               iconColor: req.query['iconColor']
+           }).then(function () {
+               res.redirect('/award')
+           });
+       }
+    });
+});
+
+router.get('/delete', function (req, res, next) {
+    var id = req.query['id'];
+    models.Award.destroy({
+        where: {
+            id: id
+    }}).then(function () {
+        res.redirect('/award')
+    })
 });
 
 module.exports = router;
