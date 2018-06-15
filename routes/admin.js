29db7912d34421a84e4ac98e8aa5832d364412e4
[Sushil Awale] <[sushil.awale@deerwalk.edu.np]>
2018-06-15 12:44:09 +0545

    Updated admin dashboard

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var sh = require('../service/sessionHandler')
+
 
 router.get('/dashboard', function(req, res, next) {
+    var sess = sh.getSession(req)
 
-    res.render('admin/dashboard', {
-            title: 'Dashboard'
-    });
+        if (sess.name === undefined) {
+            res.redirect('/?e=101'); // Not logged in
+        } else if (sess.role !== "ADMIN") {
+            res.redirect('/?e=102'); // Not authorized
+        } else {
+            res.render('admin/dashboard', {
+                title: 'Dashboard',
+                sess: sess
+            });
+        }
 });
 
 module.exports = router;
