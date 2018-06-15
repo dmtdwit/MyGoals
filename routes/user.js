b3d2e44a4abc7a8db750165686275b67a62839b9
[Sushil Awale] <[sushil.awale@deerwalk.edu.np]>
2018-06-15 12:42:24 +0545

    Updated user model, automatic role and user create

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
-var md5 = require('md5');
+// var md5 = require('md5');
+var sh = require('../service/sessionHandler');
 
 router.get('/create', function(req, res, next) {
     models.User.findAll({
     }).then(function(users) {
         res.render('user/create', {
             title: 'Create New User',
-            users: users
+            users: users,
+            sess: sh.getSession(req)
+        });
+    });
+});
+
+router.get('/edit', function(req, res, next) {
+    var id = req.query['id'];
+
+    models.User.findAll({
+    }).then(function(users) {
+        models.User.findOne({where:{id: id}}).then(function (user) {
+            res.render('user/edit', {
+                title: 'Edit User | My Goals',
+                firstName: user.name.split(" ")[0],
+                lastName: user.name.split(" ")[1],
+                user: user,
+                users: users
+            });
         });
     });
 });
@@ -31,28 +50,16 @@ router.post('/save', function(req, res, next) {
 
     models.User.create({
         name: req.body.firstName + " " + req.body.lastName,
-        password: md5(req.body.email),
+        password: req.body.email,
         email: req.body.email,
         category: category
     }).then(function(result){
         return result.setRole(role);
+    }).then(function(resultTwo){
+        return resultTwo.setManager(req.body.manager);
     });
 
-    models.User.findOne({
-        where: {
-            email: req.body.manager
-        }
-    }).then(function(result){
-        models.User.findOne({
-            where: {
-                email: req.body.email
-            }
-        }).then(function(resultTwo){
-
-        })
-    });
-
-    res.redirect("create");
+    res.redirect("/admin/dashboard");
 });
 
 router.get('/edit', function(req, res, next) {
@@ -66,7 +73,8 @@ router.get('/edit', function(req, res, next) {
 router.get('/profile', function(req, res, next) {
     res.render('user/profile',
         {
-            title: 'User Profile'
+            title: 'User Profile',
+            sess: sh.getSession(req)
         }
     );
 });
@@ -77,16 +85,18 @@ router.get('/list', function(req, res, next) {
     }).then(function(users) {
         res.render('user/list', {
             title: 'All Users',
-            users: users
+            users: users,
+            sess: sh.getSession(req)
         });
     });
 });
 
 router.get('/dashboard', function(req, res, next) {
-
+    var sess = req.session;
     res.render('user/dashboard',
         {
-            title: 'Dashboard'
+            title: 'Dashboard',
+            sess: sess
         }
     );
 
